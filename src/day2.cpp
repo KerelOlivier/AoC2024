bf8e8192dea7e087c2fd38d1c6ab0d5ba8d8e36a
Olivier Boeren <olivierboeren@live.nl>
2024-12-03 19:14:13 +0100

    add: day2 p2

--- src/day2.cpp
+++ src/day2.cpp
@@ -1,8 +1,15 @@
 #include <day2.h>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <sstream>
 #include <cmath>
+
+bool IsValid(int lhs, int rhs, int dir){
+	int d = rhs - lhs;
+	return std::signbit(d) == std::signbit(dir) && 
+		std::abs(d) >= 1 && std::abs(d) <= 3;
+}
 
 void aoc::day2::solve(char* path){
 	 // Parse input
@@ -10,42 +17,83 @@ void aoc::day2::solve(char* path){
 
 	std::string line;
 	int safe_cnt = 0;
+	int safe_cnt2 = 0;
 	while(std::getline(stream, line)){
 		std::stringstream sstream(line);
 		int curr, prev, last;
-		bool safe = true;
+		bool safe = true;	 // Safe flag for part 1
+		bool safe2 = true;	 // Safe flag for part 2
+		bool skipped = false;
+		int dir = 0;
 
-		// Initialize the orientation
 		sstream >> prev;
-		sstream >> curr;
-
-		int dir = curr - prev;
-
-		//verify first step
-		if(std::abs(dir) < 1 || std::abs(dir) > 3){
-			safe = false;
+		std::cout << prev << " ";
+		while(sstream >> curr){
+			dir+=std::signbit(curr - prev)? -1 : 1;
+			std::cout << curr << " ";
+			prev = curr;
 		}
 
-		// Move sliding window
-		last = prev;
-		prev = curr;
-
+		// Check list for validity
+		sstream = std::stringstream(line, std::ios_base::app | std::ios_base::out | std::ios_base::in);
+		sstream >> prev;
+		
+		bool first = true;
 
 		while(sstream >> curr){
-			int d = curr - prev;
-			if(std::signbit(d) != std::signbit(dir) || std::abs(d) > 3 ||
-					std::abs(d) < 1){
-				// Not a safe sequence
-				safe  = false;
+			//std::cout << "[" << curr << "]";
+			if(!IsValid(prev, curr, dir)){
+				safe = false;
+				if(!skipped){
+					int tmp;
+					if(sstream >> tmp){
+						//std::cout << prev << "-" << curr << "-" << tmp;
+						if(!IsValid(prev, tmp, dir)){ // Removing mid
+							if(first){
+								if(!IsValid(curr, tmp, dir)){
+									safe2 = false;
+								}else{
+									// drop prev
+									curr = tmp;
+								}
+							}else{
+								//std::cout << last << "-" << prev << "-" << curr <<"-" << tmp << " ";
+								if(!IsValid(last, curr, dir)){
+									safe2 = false;
+								}else{
+									// drop prev
+									prev = last;
+									//std::cout << "(" << last << "," << prev << "," << curr << "," << tmp << ")";
+								}
+								std::stringstream str;
+								str << std::to_string(tmp);
+								std::string s;
+								while(sstream >> s) str << " " << s;
+								sstream = std::stringstream(str.str());
+							}
+						}else{
+							curr = tmp;
+						}
+					}
+					skipped = true;
+				}else{
+					safe2 =false;
+				}
 			}
-
-			//Move sliding window
+			first = false;
 			last = prev;
-			prev = curr;	
+			prev = curr;
+			
 		}
 
 		if(safe) ++safe_cnt;
+		if(safe2) ++safe_cnt2;
+		std::cout << " " << std::setw(40-line.length()) << 
+			(safe ? "T" : "F") << 
+			(safe2 ? "T" : "F") << 
+			" " <<(std::signbit(dir) ? "<" : ">") << " " << dir << std::endl;
 	}
 
 	std::cout << "SOLUTION 1: " << safe_cnt << std::endl;
+	std::cout << "SOLUTION 2: " << safe_cnt2 << std::endl;
 }

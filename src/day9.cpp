d1c9d23ce8d3eb2a3348c50a304f7c1c5d2f369d
Olivier Boeren <olivierboeren@live.nl>
2024-12-09 20:21:11 +0100

    add: day 9 part 2

--- src/day9.cpp
+++ src/day9.cpp
@@ -20,7 +20,7 @@ unsigned long int Part1(std::string input){
 	}
 	unsigned long int checksum = 0;
 	int v = res.size()-1;
-	for(int u = 0; u < res.size(); ++u){
+	for(size_t u = 0; u < res.size(); ++u){
 		if(res[u] < 0){
 			while(res[v] < 0 && u < v) --v;
 			res[u] = res[v];
@@ -29,10 +29,49 @@ unsigned long int Part1(std::string input){
 	}
 
 
-	for(int u = 0; u < res.size(); ++u){
+	for(size_t u = 0; u < res.size(); ++u){
 		if(res[u] >= 0) checksum += res[u]*u;
 	}
 
+	return checksum;
+}
+
+struct Block{
+	int id;
+	int stride;
+	int size;
+};
+
+unsigned long int Part2(std::string input){
+	std::vector<Block> files;
+	std::vector<Block> free;
+	int stride = 0;
+	for(int i =0; i < input.length(); ++i){
+		int size = input[i] - '0';
+		if(i%2 == 0){
+			files.push_back({i/2, stride, size});
+		}else{
+			free.push_back({-1, stride, size});
+		}
+		stride += size;
+	}
+
+	for(int i = files.size()-1; i >= 0; --i){
+		for(int j = 0; j < free.size(); ++j){
+			if(free[j].size >= files[i].size && free[j].stride < files[i].stride){
+				free[j].size -= files[i].size;
+				files[i].stride = free[j].stride;
+				free[j].stride += files[i].size;
+			}
+		}
+	}
+
+	unsigned long int checksum = 0;
+	for(const Block& b: files){
+		for(int i = b.stride; i < b.stride + b.size; ++i){
+			checksum += b.id * i;
+		}
+	}
 	return checksum;
 }
 
@@ -41,9 +80,9 @@ void aoc::solutions::day9(char* path){
 
 	std::string line;
 	std::getline(fs, line);
+	unsigned long int a = Part1(line);
+	unsigned long int b = Part2(line);
 
-	unsigned long int res = Part1(line);
-
-	std::cout << "SOLUTION 1: " << res << std::endl;
-	std::cout << "SOLUTION 2: " << std::endl;
+	std::cout << "SOLUTION 1: " << a << std::endl;
+	std::cout << "SOLUTION 2: " << b << std::endl;
 }

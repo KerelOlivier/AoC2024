1fe87663c85fa1557ff3eb7b5440679775468fef
Olivier Boeren <olivierboeren@live.nl>
2024-12-09 17:53:31 +0100

    add: day 8 part 2

--- src/day8.cpp
+++ src/day8.cpp
@@ -28,23 +28,67 @@ std::unordered_set<size_t> GetDistinctAntinodes(
 				int u = towers[j].x - dx;
 				int v = towers[j].y - dy;
 				
-				std::cout << towers[i].freq << " [" << towers[i].x << "," << towers[i].y << "] ["
-						         << towers[j].x << "," << towers[j].y << "]" << std::endl; 
+				//std::cout << towers[i].freq << " [" << towers[i].x << "," << towers[i].y << "] ["
+				//		         << towers[j].x << "," << towers[j].y << "]" << std::endl; 
 
 				if(x >= 0 && x < width && y >=0 && y < height){
 					nodes.insert(y * width + x);
-					std::cout << right << "[" << x << "," << y << "] " << (y * width * x) << std::endl;
+					//std::cout << right << "[" << x << "," << y << "] " << (y * width * x) << std::endl;
 				}else{
-					std::cout << wrong << "[" << x << "," << y << "]" << std::endl;
+					//std::cout << wrong << "[" << x << "," << y << "]" << std::endl;
 				}
 				if(u >= 0 && u < width && v >=0 && v < height){
 					nodes.insert(v * width + u);
-					std::cout << right << "[" << u << "," << v << "] " << (v * width * u) << std::endl;
+					//std::cout << right << "[" << u << "," << v << "] " << (v * width * u) << std::endl;
 				}else{
-					std::cout << wrong << "[" << u << "," << v << "]" << std::endl;
+					//std::cout << wrong << "[" << u << "," << v << "]" << std::endl;
 				}
 
 
+			}
+		}
+	}
+
+	return nodes;
+}
+
+std::unordered_set<size_t> GetDistinctAntinodes2(
+		std::vector<Tower> towers, size_t width, size_t height){
+	std::unordered_set<size_t> nodes;
+
+	for(size_t i = 0; i < towers.size(); ++i){
+		nodes.insert(towers[i].y * width + towers[i].x);
+		for(size_t j = i + 1; j < towers.size(); ++j){
+			if(towers[i].freq == towers[j].freq){
+				std::cout << towers[i].freq << " [" << towers[i].x << "," << towers[i].y << "] ["
+				          << towers[j].x << "," << towers[j].y << "]" << std::endl; 
+
+
+				int dx = (int)towers[i].x - (int)towers[j].x;
+				int dy = (int)towers[i].y - (int)towers[j].y;
+
+				int x = towers[i].x + dx;
+				int y = towers[i].y + dy;
+
+				while(x >= 0 && x < width && y >= 0 && y < height){
+					nodes.insert(y * width + x);
+					std::cout << "[" << x << "," << y << "]" << std::endl;
+
+					x += dx;
+					y += dy;
+				}
+
+				int u = towers[j].x - dx;
+				int v = towers[j].y - dy;
+
+				while(u >= 0 && u < width && v >= 0 && v < height){
+					nodes.insert(v * width + u);
+					std::cout << "[" << u << "," << v << "]" << std::endl;
+
+					u -= dx;
+					v -= dy;
+				}
+				
 			}
 		}
 	}
@@ -74,5 +118,6 @@ void aoc::solutions::day8(char* path){
 	std::cout << width << " " << height << std::endl;
 	size_t nodes = GetDistinctAntinodes(towers, width, height).size();
 	std::cout << "SOLUTION 1: " << nodes << std::endl;
-	std::cout << "SOLUTION 2: " << std::endl;
+	size_t nodes2 = GetDistinctAntinodes2(towers, width, height).size();
+	std::cout << "SOLUTION 2: " << nodes2 << std::endl;
 }

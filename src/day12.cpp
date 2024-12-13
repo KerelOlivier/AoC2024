ab928df45544270be9575f078e5afdfcaff30aec
Olivier Boeren <olivierboeren@live.nl>
2024-12-13 04:37:39 +0100

    add: day 12 part 2

--- src/day12.cpp
+++ src/day12.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <queue>
 #include <solutions.h>
+#include <strstream>
 
 struct Region{
 	int area;
@@ -48,7 +49,101 @@ Region Floodfill(int id, int width, int height,
 }
 
 int Part2(int width, int height, std::vector<std::string>& map){
-	return 0;
+	std::vector<int> regions(width*height, -1);
+	std::vector<int> areas;
+	// Properly ID regions
+	for(int row = 0; row < height; ++row){
+		for(int col = 0; col < width; ++col){
+			int id = row * width + col;
+			if(regions[id] < 0){
+				int area = 0;
+				//Floodfill and update region
+				std::queue<int> cells;
+				cells.push(id);
+
+				while(!cells.empty()){
+					int cell = cells.front();
+					int row = cell / width;
+					int col = cell % width;
+					cells.pop();
+
+
+					if(regions[cell] != -1) continue;
+					regions[cell] = areas.size();
+
+					++area;
+
+					// Add unvisited neighbours of same region
+					char c = map[row][col];
+					if(col + 1 < width && map[row][col+1] == c)	cells.push(cell+1);
+					if(col - 1 >= 0 && map[row][col-1] == c) cells.push(cell-1);
+					if(row + 1 < height && map[row+1][col] == c) cells.push(cell+width);
+					if(row - 1 >= 0 && map[row-1][col] == c) cells.push(cell-width);
+
+				}
+				areas.push_back(area);
+			}
+		}
+	}
+
+	std::vector<int> perimeters(areas.size(), 0);
+	// Find horizonal Fences
+	for(int row = 0; row < height; ++row){
+
+		for(int col = 0; col < width; ++col){
+			int id = row * width + col;
+			int tl = row == 0 || col == 0 ? -1 : regions[id-width-1];
+			int t = row == 0 ? -1 : regions[id-width];
+			int ml = col == 0 ? -1 : regions[id-1];
+			int m = regions[id];
+			int bl = row == height - 1 || col == 0? -1 : regions[id+width-1];
+			int b = row == height ? -1 : regions[id+width];
+			
+			if(row == 0 || m != t){ // Up
+				if(tl == m || ml != m){
+					++perimeters[regions[id]];
+				}
+			}
+			if(row == height - 1 || m != b){ // Down
+				if(bl == m || ml != m){
+					++perimeters[regions[id]];
+				}
+			}
+		
+		}
+	}
+	for(int col = 0; col < width; ++col){
+		for(int row = 0; row < height; ++row){
+			int id = row * width + col;
+			
+			int t = row == 0 ? -1 : regions[id-width];
+			int m = regions[id];
+
+			int tr = row == 0 || col == width - 1 ? -1 : regions[id+1-width];
+			int mr = col == width - 1 ?  - 1 : regions[id+1];
+
+			int tl = row == 0 || col == 0 ? -1 : regions[id-width-1];
+			int ml = col == 0 ? -1 : regions[id-1];
+			
+			if(col == 0 || m != ml){ // Left
+				if(tl == m || t != m){
+					++perimeters[regions[id]];
+				}
+			}
+			if(col == width - 1 || m != mr){ // Right
+				if(tr == m || t != m){
+					++perimeters[regions[id]];
+				}
+			}
+		
+		}
+	}
+
+	int res = 0;
+	for(size_t i = 0;i < perimeters.size(); ++i){
+		res += perimeters[i]*areas[i];
+	}
+	return res;
 }
 
 void aoc::solutions::day12(char* path){
@@ -58,7 +153,6 @@ void aoc::solutions::day12(char* path){
 
 	while(getline(fs, line)){
 		field.push_back(line);
-		std::cout << line << std::endl;
 	}
 
 	int width = field[0].size();
@@ -73,8 +167,6 @@ void aoc::solutions::day12(char* path){
 			int id = row * width + col;
 			if(!visited[id]){
 				Region region = Floodfill(id, width, height, field, visited);
-				std::cout << "{" << region.c << "," << region.area << "," 
-					      << region.perimeter << "}" <<std::endl;
 				cost += region.area * region.perimeter;
 			}
 		}
